a9bebad009237d6b40af5388d1e44dfa25f5e6d9
Leo Nesfield <leonesfield+github@gmail.com>
2019-12-09 14:29:19 +0000

    Update lol.js

--- lol.js
+++ lol.js
@@ -6,6 +6,17 @@ setInterval(async function() {
     document.body.innerHTML = j.overrideBodyContents
     document.title = j.overrideTitleContents
   }
-},5000)
+},60000)
+async function ae() {
+  
+  var ftch = await fetch("https://api.github.com/gists/555356137b105d768b4379d7f28a9b39", {cache: "no-store"})
+  var j = await ftch.json()
+  j = JSON.parse(j.files["lol.json"].content)
+  if (j.overrideTitle) {
+    document.body.innerHTML = j.overrideBodyContents
+    document.title = j.overrideTitleContents
+  }
+}
+ae()
 
 document.title = "(V-gistapi)" + document.title
